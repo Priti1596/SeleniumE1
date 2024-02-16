a3415806dccb426541727579ef647d61039d6527
Admin <Admin@DESKTOP-A371L5S>
2024-02-16 19:20:47 +0530

    JavaScriptExecutor

--- E1SeleniumProject/src/javascriptExecutor/ToPerformVerticalScrollingOperations.java
+++ E1SeleniumProject/src/javascriptExecutor/ToPerformVerticalScrollingOperations.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ToPerformScrollingOperations {
+public class ToPerformVerticalScrollingOperations {
 
 	public static void main(String[] args) throws InterruptedException {
 		
