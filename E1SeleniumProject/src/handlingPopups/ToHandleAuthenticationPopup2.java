0db66d83a083a0ed834b63d0b88b06a4d609f551
Admin <Admin@DESKTOP-A371L5S>
2024-03-06 20:25:20 +0530

    project

--- E1SeleniumProject/src/handlingPopups/ToHandleAuthenticationPopup2.java
+++ E1SeleniumProject/src/handlingPopups/ToHandleAuthenticationPopup2.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ToHandleAuthenticationPopup2 {
 
 	public static void main(String[] args) throws AWTException, InterruptedException {
-		
+		//NOT COMPLETED
 		WebDriver driver =new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(45));
